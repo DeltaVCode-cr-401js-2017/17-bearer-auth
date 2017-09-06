9e95c0f4d0859bc195d5286441c16745a6975afc
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-06 16:01:55 -0500

    bearer auth middleware set up

---
+++ lab-beeksma/lib/bearer-auth-middleware.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const debug = require('debug')('app:bearer-auth-middleware');
+
+const User = require('../model/user');
+
+module.exports = function (req, res, next) {
+  debug('bearer-auth-middleware');
+  let authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return next(createError(401, 'authorization header missing'));
+  }
+  let token = authHeader.split('Bearer ')[1];
+  if (!token) {
+    return next(createError(401, 'token messing'));
+  }
+
+  jwt.verify(token, process.env.APP_SECRET, (err, decoded) => {
+    if (err) return next(err);
+    debug('decoded token', decoded);
+
+    User.findOne({ findHash: decoded.token })
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        next(createError(401, err.message));
+      });
+  });
+};
