8d1ba42ee2ec9fee9fde9160e14f0fc4a62e0f5f
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-06 16:58:44 -0500

    tests for PUT to /api/gallery/;id

--- lab-beeksma/test/gallery-route.test.js
+++ lab-beeksma/test/gallery-route.test.js
@@ -19,6 +19,11 @@ const exampleGallery = {
   ,desc: 'some stuff goes here'
 };
 
+const updatedGallery = {
+  name: 'updated test gallery'
+  ,desc: 'look new stuff'
+};
+
 describe('Gallery Routes', function (){
   beforeEach(function () {
     return User.createUser(exampleUser)
@@ -131,6 +136,79 @@ describe('Gallery Routes', function (){
       it('should return 401', function () {
         return request
           .get(`/api/gallery/${this.testGallery._id}`)
+          .set({'Authorization': `Bearer ${this.wrongToken}`})
+          .expect(401);
+      });
+    });
+  });
+
+  describe('PUT /api/gallery/:id', function (){
+    describe('valid id and body', function(){
+      it('should return a gallery with updated values', function (){
+        return request
+          .put(`/api/gallery/${this.testGallery.id}`)
+          .set({'Authorization': `Bearer ${this.testToken}`})
+          .send(updatedGallery)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.name).to.equal(updatedGallery.name);
+            expect(res.body).to.have.property('desc', updatedGallery.desc);
+          });
+      });
+    });
+    describe('valid id and invalid body', function(){
+      it('should return 400 if no body sent', function (){
+        return request
+          .put(`/api/gallery/${this.testGallery.id}`)
+          .set({'Authorization': `Bearer ${this.testToken}`})
+          .expect(400);
+      });
+      it('should return 400 if invalid body sent', function (){
+        return request
+          .put(`/api/gallery/${this.testGallery.id}`)
+          .set({'Authorization': `Bearer ${this.testToken}`})
+          .send('look ma no JSON')
+          .expect(400);
+      });
+    });
+    describe('invalid id', function(){
+      it('should return 404', function (){
+        return request
+          .put('/api/gallery/noID')
+          .send(updatedGallery)
+          .set({'Authorization': `Bearer ${this.testToken}`})
+          .expect(404);
+      });
+    });
+    describe('valid looking id not found', function(){
+      it('should return 404', function (){
+        return request
+          .put('/api/gallery/thislookslikeanid1234567')
+          .send(updatedGallery)
+          .set({'Authorization': `Bearer ${this.testToken}`})
+          .expect(404);
+      });
+    });
+    describe('No token sent', function(){
+      it('should return 401', function (){
+        return request
+          .put('/api/gallery/thislookslikeanid1234567')
+          .send(updatedGallery)
+          .set({'Authorization': `Bearer `})
+          .expect(401);
+      });
+    });
+    describe(`someone else's gallery`, function () {
+      before(function () {
+        return User.createUser({ username: 'JonnyWrong', email: 'wrong@example.com', password: 'oops' })
+          .then(wrong => this.wrong = wrong)
+          .then(wrong => wrong.generateToken())
+          .then(wrongToken => this.wrongToken = wrongToken);
+      });
+      it('should return 401', function () {
+        return request
+          .put(`/api/gallery/${this.testGallery._id}`)
+          .send(updatedGallery)
           .set({'Authorization': `Bearer ${this.wrongToken}`})
           .expect(401);
       });
